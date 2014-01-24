f2e0ee77a13ff2313112b4478e9bcd5c8c0cefde
Diego Polido Santana <diego@polidosantana.com.br>
2014-01-24 16:14:57 -0200

    Project Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImaBold::Application.config.secret_key_base = 'b5f7cdb0ec2542de39110687bfe31d17cac8c8a2d08b02c363ec546e8ef818a412a2cb416e8f3f10ed6c92424f1e53c51ae783ae4b1c785ac9b0ab1c78ee8dee'
